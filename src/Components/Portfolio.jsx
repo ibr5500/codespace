9d41a82db0f10450adda249bc60097218ffa556a
IBRAHIM AHMAT <ibr.s.ahmat@gmail.com>
2024-01-24 05:17:54 +0000

    ee

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -55,25 +55,16 @@ const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
-            src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
-            alt={imageAltText}
-          />
-        </div>
-        <div className="container">
-          {projectList.map((project) => (
-            <div className="box" key={project.title}>
-              <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
-              </a>
-              <p className="small">{project.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <img
+        src={image}
+        alt={imageAltText}
+        style={{
+          height: "90%",
+          width: "100%",
+          objectFit: "cover",
+          animation: "1s ease-out 0s 1 slideIn",
+        }}
+      />
     </section>
   );
 };
